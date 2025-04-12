0ad8877f0dfaec6dae3b015dd4b9cab170fe61e3
Jianzhu Huai <huai.3@osu.edu>
2025-04-12 20:51:42 +0800

    replace sys/timeb.h with std::chrono and std::time_t

--- src/HesaiLidar_General_SDK/src/PandarGeneralRaw/include/log.h
+++ src/HesaiLidar_General_SDK/src/PandarGeneralRaw/include/log.h
@@ -14,50 +14,67 @@
  * limitations under the License.
  *****************************************************************************/
 
-#include <stdio.h>
+#include <cstdio>
 #include <unistd.h>
 #include <thread>
-#include <sys/time.h>
-#include <sys/timeb.h>
+#include <chrono>
+#include <ctime>
 
-class TranceFunc
-{
+inline void get_current_time(int& hour, int& min, int& sec, int& millis) {
+    using namespace std::chrono;
+
+    auto now = system_clock::now();
+    auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+    std::time_t tt = system_clock::to_time_t(now);
+    struct tm *ptm = localtime(&tt);
+
+    hour = ptm->tm_hour;
+    min = ptm->tm_min;
+    sec = ptm->tm_sec;
+    millis = static_cast<int>(ms.count());
+}
+
+class TranceFunc {
 public:
-    TranceFunc(const char* file, const char* func){
-        m_cFile = file;
-        m_cFunc = func;
-        struct tm *ptm;
-        struct timeb stTimeb;
-        ftime(&stTimeb);
-        ptm = localtime(&stTimeb.time);
-        printf("[T] %02d:%02d:%02d.%03d pid:%d tid:%10d ->[File:%s Function:%s ]\n", ptm->tm_hour, ptm->tm_min, ptm->tm_sec, stTimeb.millitm, getpid(), std::hash<std::thread::id>()(std::this_thread::get_id()), m_cFile, m_cFunc);
+    TranceFunc(const char* file, const char* func)
+        : m_cFile(file), m_cFunc(func) {
+        int hour, min, sec, millis;
+        get_current_time(hour, min, sec, millis);
+        printf("[T] %02d:%02d:%02d.%03d pid:%d tid:%10zu ->[File:%s Function:%s ]\n",
+               hour, min, sec, millis, getpid(),
+               std::hash<std::thread::id>()(std::this_thread::get_id()),
+               m_cFile, m_cFunc);
     }
-    ~TranceFunc(){
-        struct tm *ptm;
-        struct timeb stTimeb;
-        ftime(&stTimeb);
-        ptm = localtime(&stTimeb.time);
-        printf("[T] %02d:%02d:%02d.%03d pid:%d tid:%10d <-[File:%s Function:%s ]\n", ptm->tm_hour, ptm->tm_min, ptm->tm_sec, stTimeb.millitm, getpid(), std::hash<std::thread::id>()(std::this_thread::get_id()), m_cFile, m_cFunc);
+
+    ~TranceFunc() {
+        int hour, min, sec, millis;
+        get_current_time(hour, min, sec, millis);
+        printf("[T] %02d:%02d:%02d.%03d pid:%d tid:%10zu <-[File:%s Function:%s ]\n",
+               hour, min, sec, millis, getpid(),
+               std::hash<std::thread::id>()(std::this_thread::get_id()),
+               m_cFile, m_cFunc);
     }
+
     const char* m_cFile;
     const char* m_cFunc;
 };
 
-#define LOG_D(format,...) {\
-    struct tm *ptm;\
-    struct timeb stTimeb;\
-    ftime(&stTimeb);\
-    ptm = localtime(&stTimeb.time);\
-    printf("[D] %02d:%02d:%02d.%03d pid:%d tid:%10d File:%s Function:%s Line:%d " format"\n", ptm->tm_hour, ptm->tm_min, ptm->tm_sec, stTimeb.millitm, getpid(), std::hash<std::thread::id>()(std::this_thread::get_id()), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);\
+#define LOG_D(format, ...) {\
+    int hour, min, sec, millis;\
+    get_current_time(hour, min, sec, millis);\
+    printf("[D] %02d:%02d:%02d.%03d pid:%d tid:%10zu File:%s Function:%s Line:%d " format "\n",\
+           hour, min, sec, millis, getpid(),\
+           std::hash<std::thread::id>()(std::this_thread::get_id()),\
+           __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);\
 }
 
-#define LOG_E(format,...) {\
-    struct tm *ptm;\
-    struct timeb stTimeb;\
-    ftime(&stTimeb);\
-    ptm = localtime(&stTimeb.time);\
-    printf("[E] %02d:%02d:%02d.%03d pid:%d tid:%10d File:%s Function:%s Line:%d " format"\n", ptm->tm_hour, ptm->tm_min, ptm->tm_sec, stTimeb.millitm, getpid(), std::hash<std::thread::id>()(std::this_thread::get_id()), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);\
+#define LOG_E(format, ...) {\
+    int hour, min, sec, millis;\
+    get_current_time(hour, min, sec, millis);\
+    printf("[E] %02d:%02d:%02d.%03d pid:%d tid:%10zu File:%s Function:%s Line:%d " format "\n",\
+           hour, min, sec, millis, getpid(),\
+           std::hash<std::thread::id>()(std::this_thread::get_id()),\
+           __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);\
 }
 
-#define LOG_FUNC() TranceFunc tf( __FILE__, __FUNCTION__)
-    
+#define LOG_FUNC() TranceFunc tf(__FILE__, __FUNCTION__)
